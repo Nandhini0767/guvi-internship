d0167cd1add0a3a34557dd995e5b473b64bf8816
Nandhini G <nandhini0767@gmail.com>
2026-07-04 18:10:13 +0530

    Added GUVI Internship Project with Redis session integration

---
+++ README.md
@@ -0,0 +1,54 @@
+# GUVI Internship Project
+
+## User Authentication System
+
+This project was developed as part of the GUVI Internship.
+
+## Features
+
+- User Registration
+- User Login
+- User Profile
+- Session Management
+- MongoDB Integration
+- MySQL Database Support
+- Redis Support
+- AJAX and jQuery
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- AJAX
+- jQuery
+- PHP
+- MySQL
+- MongoDB
+- Redis
+- Composer
+
+## Project Structure
+
+- assets/
+- php/
+- vendor/
+- composer.json
+- composer.lock
+- db.php
+- guvi.sql
+- php.ini
+
+## How to Run
+
+1. Install XAMPP.
+2. Start Apache and MySQL.
+3. Import `guvi.sql` into MySQL.
+4. Install and start MongoDB.
+5. Install and start Redis.
+6. Place the project inside the `htdocs` folder.
+7. Open the project in your browser.
+
+## Author
+
+**Nandhini G**
---
+++ guvi.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 03, 2026 at 05:44 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `guvi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `contact` varchar(20) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`id`, `name`, `age`, `dob`, `contact`, `email`) VALUES
+(32, 'Nandhini G', 21, '2005-02-26', '8015554767', 'nandhu@test.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`) VALUES
+(7, 'Nandhini G', 'nandhini@test.com', '$2y$10$gWuMjlPW9Tp2OeZiLb5tT.zUN6bEFfSdHCq9sKqR67sdUjAFn4Zyu', '2026-07-03 09:37:15', '2026-07-03 09:37:15');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
