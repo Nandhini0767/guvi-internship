270b9e1b0a5828cfe5e294c6142dca6879ef61f9
Nandhini G <nandhini0767@gmail.com>
2026-07-10 18:54:03 +0530

    update register.js

--- assets/js/register.js
+++ assets/js/register.js
@@ -19,7 +19,24 @@ $(document).ready(function () {
 
                 alert(response);
 
+                // Clear the form
                 $("#registerForm")[0].reset();
+
+                // If registration was successful,
+                // automatically go to the login page
+                if (response.includes("Registration Successful")) {
+
+                    setTimeout(function () {
+                        window.location.href = "login.html";
+                    }, 1000); // Redirect after 1 second
+
+                }
+
+            },
+
+            error: function () {
+
+                alert("Server Error!");
 
             }
 
